c8fb8d80d63b1ba2232d7342730951790ad48638
JMcunst <xnslqjtmghaf@gmail.com>
2024-01-10 22:10:47 +0900

    feat : screentuil % getx

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
+import 'package:grocery_clone/constants/constants.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    //Set the fit size (Find your UI design, look at the dimensions of the device screen and fill it in,unit in dp)
+    return ScreenUtilInit(
+      designSize: const Size(375, 825),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      // Use builder only if you need to use library outside ScreenUtilInit context
+      builder: (context , child) {
+        return GetMaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: 'Foodly',
+          // You can use the library anywhere in the app even in theme
+          theme: ThemeData(
+            scaffoldBackgroundColor: kOffWhite,
+            iconTheme: const IconThemeData(color: kDark),
+            primarySwatch: Colors.grey
+          ),
+          home: child,
+        );
+      },
+      child: const MyHomePage(title: 'First Methods'),
     );
   }
 }
