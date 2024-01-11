a53e575df9c9c497abbe109502e3433c0c86102e
jmcunst <xnslqjtmghaf@gmail.com>
2024-01-11 12:27:15 +0900

    feat : heading widget

--- lib/views/home/home_page.dart
+++ lib/views/home/home_page.dart
@@ -5,26 +5,36 @@ import 'package:grocery_clone/common/custom_container.dart';
 import 'package:grocery_clone/constants/constants.dart';
 import 'package:grocery_clone/views/home/widgets/category_list.dart';
 
+import '../../common/heading.dart';
+
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: kPrimary,
-      appBar: PreferredSize(
-        preferredSize: Size.fromHeight(130.h),
-        child: CustomAppbar()
-      ),
-      body: SafeArea(
-        child: CustomContainer(
-          containerContent: Column(
+        backgroundColor: kPrimary,
+        appBar: PreferredSize(
+            preferredSize: Size.fromHeight(130.h), child: CustomAppbar()),
+        body: SafeArea(
+          child: CustomContainer(
+              containerContent: Column(
             children: [
-              CategoryList()
+              CategoryList(),
+              Heading(
+                text: "Nearby Restaurants",
+                onTap: () {},
+              ),
+              Heading(
+                text: "Try Something New",
+                onTap: () {},
+              ),
+              Heading(
+                text: "Food Closer to you",
+                onTap: () {},
+              )
             ],
-          )
-        ),
-      )
-    );
+          )),
+        ));
   }
 }
